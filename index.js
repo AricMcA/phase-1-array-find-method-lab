b4c9581980284f107cbccb2106829ffa2f50c213
Aric McAlister <aricmca@gmail.com>
2021-06-17 18:07:16 -0500

    commit tree df96578d46fb5ea9e9ac8c2908b2e1505e719cd5

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const record = [
+    { year: "1969", result: "peanut"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"},
+    { year: "1966", result: "L"},
+]
+
+const year = record.find(superbowlWin).year;
+
+function superbowlWin(outcome) {
+    return outcome.result === 'peanut';
+}
+
+console.log(year);
 
